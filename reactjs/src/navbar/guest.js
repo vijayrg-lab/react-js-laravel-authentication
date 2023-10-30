0f71e558a25820d4aa3cafd89c30955939c8d2dc
vijayrg-lab <141930785+vijayrg-lab@users.noreply.github.com>
2023-10-30 13:04:00 +0530

    Add files via upload

---
+++ reactjs/src/navbar/guest.js
@@ -0,0 +1,44 @@
+import Users from '../Users';
+import {Routes, Route, Link } from "react-router-dom";
+import Home from '../pages/Home'
+import Create from '../pages/create';
+import Edit from '../pages/Edit';
+import View from '../pages/View';
+import Login from '../pages/Login';
+import Register from '../pages/register';
+
+
+function Guest() {
+    return (
+        <>
+            <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">Home</Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to={"/create"} className="nav-link">Create</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/login">Login</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/register">Register</Link>
+                    </li>
+
+                </ul>
+
+            </nav>
+            <div className="container">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/create" element={<Create />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </div>
+        </>
+    );
+}
+
+export default Guest;
