2ca4a5164f30006f80eee2bc6ed643f3192ca17c
vijayrg-lab <141930785+vijayrg-lab@users.noreply.github.com>
2023-10-30 13:03:44 +0530

    Create auth.js

---
+++ reactjs/src/navbar/auth.js
@@ -0,0 +1,39 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from '../pages/Home';
+import Dashboard from '../pages/Dashboard';
+import AuthUser from '../pages/AuthUser';
+function Auth() {
+    const {token,logout} = AuthUser();
+    const logoutUser = () => {
+        if(token != undefined){
+            logout();
+        }
+    }
+    return (
+        <>
+            <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/dashboard">Dashboard</Link>
+                    </li>
+                    <li className="nav-item">
+                        <span role="button" className="nav-link" onClick={logoutUser}>Logout</span>
+                    </li>
+
+                </ul>
+
+            </nav>
+            <div className="container">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                </Routes>
+            </div>
+        </>
+    );
+}
+
+export default Auth;
