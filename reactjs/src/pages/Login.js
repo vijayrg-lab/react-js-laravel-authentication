a871a408b772df8a0b2d95a830206ad66f70e387
vijayrg-lab <141930785+vijayrg-lab@users.noreply.github.com>
2023-10-30 13:05:37 +0530

    Add files via upload

---
+++ reactjs/src/pages/Login.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { useState } from "react";
+import AuthUser from "./AuthUser";
+
+
+export default function Login(){
+
+    const{http,setToken} = AuthUser();
+    const[email,setEmail] = useState();
+    const[password,setPassword] = useState();
+
+    const submitForm = async() => {
+        console.log(email + ' ' + password);
+
+        //api call
+        http.post('login',{email:email,password:password}).then((res)=>{
+            // console.log(res.data);
+            setToken(res.data.user,res.data.access_token);
+        })
+    }
+
+
+
+    return(
+        <div className="row justify-content-center pt-5">
+            <div className="col-sm-6">
+                <div className="card p-4">
+                    <h1 className="text-center mb-3">Login </h1>
+                    <div className="form-group">
+                        <label>Email address:</label>
+                        <input type="email" className="form-control" placeholder="Enter email" 
+                            onChange={e=>setEmail(e.target.value)}
+                        id="email" />
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>Password:</label>
+                        <input type="password" className="form-control" placeholder="Enter password" 
+                            onChange={e=>setPassword(e.target.value)}
+                        id="pwd" />
+                    </div>
+                    
+                    <button type="submit" onClick={submitForm} className="btn btn-primary">Submit</button>
+                </div>
+            </div>
+        </div>
+    )
+}
